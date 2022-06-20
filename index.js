d338cfdf50b43a70c2d8f158207941ebde4635ee
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-06-20 00:21:58 -0300

    fix: registration implemented

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 
-const app = express(); 
+const server = express(); 
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+server.use(express.json());
+server.use(cors());
+
+//global variables 
+const users = [];
+const tweets = [];
+
+server.post('/sign-up', (request, response) => {
+  const {username, avatar} = request.body;
+
+  if(!username || !avatar) {
+    response.status(400).send("Verifique. Todos os campos são obrigatórios!");
+  } else if(users.find((user) => user.username === username)) {
+    response.status(400).send("Este usuário já existe. Por favor escolha outro.");
+  } else {
+    users.push({username, avatar});
+    response.status(200).send("Ok! Usuário cadastrado!");
+  }
+
 });
 
-app.listen(4000);
+server.listen(5000);
