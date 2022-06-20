06d24f9b0725a7b8e167ff36c092ddb32aa427c7
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-06-20 11:23:18 -0300

    fix: getting the tweets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import chalk from 'chalk';
 import cors from 'cors';
 
 const server = express(); 
@@ -24,4 +25,28 @@ server.post('/sign-up', (request, response) => {
 
 });
 
-server.listen(5000);
+server.get('/tweets', (request, response) => {
+  const page = request.query.page;
+
+  if(Number.isInteger(parseInt(page, 10))) {
+    let numberFilterTweets = 10;
+
+    let min = tweets.length - page * (numberFilterTweets + 1);
+    let max = tweets.length - (page -1) * (numberFilterTweets + 1);
+
+    const pageTweets = tweets.filter((tweet, index) => {
+      return index > min && index <= max;
+    }); 
+
+    const invertedOfTweets = [...pageTweets].reverse();
+
+    response.send(invertedOfTweets);
+  } else {
+    response.status(400).send("Verifique a página!")
+  }
+
+});
+
+server.listen(5000, () => {
+  console.log(chalk.bold.green("Servidor funcionando na porta 5000"))
+});
